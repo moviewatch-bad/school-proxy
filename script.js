2b626c33c907bd141ffafa83847cc2571e2be87e
Dragon_X <barron.samuel111@gmail.com>
2025-01-02 13:23:39 -0800

    Updated Names

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // This is the function to open the Slope game V
 function openSlopeGame() {
-    window.location.href = "games/slope/Slope-Game-main/index.html";
+    window.location.href = "games/slope/Slope/index.html";
 }
 

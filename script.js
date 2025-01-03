a17832d72afff078881cc0f99fec1f8bff7e6e4d
TechWalrus <137485792+WalrusWalrup@users.noreply.github.com>
2025-01-03 12:20:37 +1100

    Update script.js

--- script.js
+++ script.js
@@ -2,4 +2,7 @@
 function openSlopeGame() {
     window.location.href = "games/slope/slope/index.html";
 }
-
+// This is the function to open Snow-Rider-3D game V
+function openSnowrider3dGame() {
+    window.location.href = "games/snow-rider-3d/index.html";
+}
